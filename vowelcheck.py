dc704d46626af5e5be81b4494e95a62d9ae9660d
Sneha369 <157492674+Saini32@users.noreply.github.com>
2024-11-26 21:56:10 +0530

    Add files via upload

---
+++ vowelcheck.py
@@ -0,0 +1,7 @@
+def vchk(v):
+    if(v=="a" or v=="A" or v=="e" or v=="E" or v=="i" or v=="I" or v=="o" or v=="O" or v=="u" or v=="U" ):
+        print("vowels")
+    else:
+        print("not vowels")
+v=input("enter any character=")
+vchk(v)
